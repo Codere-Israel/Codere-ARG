4e01f439bc599e6cf59c82ae2abe7875064cdb20
Avner-Levy-Cod <108326496+Avner-Levy-Cod@users.noreply.github.com>
2022-10-31 16:12:50 +0200

    Inicio de Argentina first Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,338 @@
+import {
+  Button,
+  Container,
+  Nav,
+  Navbar,
+  Dropdown,
+  Modal,
+  Form,
+  Accordion,
+} from "react-bootstrap";
+import { isMobileContext } from "../App";
+import React, { useState, useEffect } from "react";
+// import Axios from "axios";
+import { elastic as Menu } from "react-burger-menu";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleDown,
+  faAngleRight,
+  faGlobe,
+  faCirclePlay,
+  faBullhorn,
+  faCrosshairs,
+  faCoins,
+  faFlag,
+  faStar,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
+
+class PostUser {
+  constructor(loginName, password) {
+    this.loginName = loginName;
+    this.password = password;
+    // this.persistCookie = persistCookie;
+    // this.deviceType = deviceType;
+  }
+}
+
+function Header(props) {
+  const acceder =
+    "https://m.caba.codere.bet.ar/deportes/#/HomePage?openlogin=true";
+
+  // const URL = "https://dm.apuestas.codere.es/LoginServicesESApi/login/web";
+  const LOGO =
+    new Date() <= new Date("11/02/2022")
+      ? "https://www.codere.bet.ar/_catalogs/masterpage/codere/images/splash/halloweenLogo.png"
+      : "https://www.codere.bet.ar/_catalogs/masterpage/codere/images/Splash/riverPlateTopLogo.svg";
+
+  // Hooks
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState("");
+  const [pw, setPassword] = useState("");
+  const [hamburger, setHamburger] = useState(false);
+
+  // Handlers
+  // const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
+
+  // const formHandler = (evt) => {
+  //   evt.preventDefault();
+  //   // console.log("been there done that");
+
+  //   checkExistingUser();
+
+  //   setEmail("");
+  //   setPassword("");
+  // };
+
+  // const emailHandler = (evt) => {
+  //   console.log(evt.target.value);
+  //   setEmail(evt.target.value);
+  // };
+
+  // const pwHandler = (evt) => {
+  //   console.log(evt.target.value);
+  //   setPassword(evt.target.value);
+  // };
+
+  // function checkExistingUser() {
+  //   Axios.post(URL, new PostUser(email, pw)).then((res) => {
+  //     console.log("users >>");
+  //     console.log(res.data);
+  //   });
+  // }
+
+  const hamburgerHandler = (evt) => {
+    let flag = false;
+    // console.log(evt.target.className);
+    if (!hamburger && evt.target.id.includes("react-burger-menu-btn")) {
+      evt.target.parentNode.classList.add("open");
+      flag = true;
+    } else if (evt.target.id.includes("react-burger-menu-btn")) {
+      evt.target.parentNode.classList.remove("open");
+      flag = true;
+    } else if (
+      evt.target.className.includes("bm-overlay") ||
+      evt.target.id.includes("react-burger-cross-btn")
+    ) {
+      document
+        .getElementById("react-burger-menu-btn")
+        .parentNode.classList.remove("open");
+      flag = true;
+    }
+    if (flag) setHamburger(!hamburger);
+  };
+
+  return (
+    <isMobileContext.Consumer>
+      {(isMobile) => {
+        return (
+          <Navbar className="header_nav stroke" variant="dark">
+            {isMobile ? (
+              <div id="father">
+                <div id="outer-container" onClick={hamburgerHandler}>
+                  <Menu
+                    id="elastic"
+                    left
+                    customCrossIcon={false}
+                    pageWrapId={"page-wrap"}
+                    outerContainerId={"outer-container"}
+                  >
+                    {/* <a href="#">
+                  <FontAwesomeIcon icon={faRightToBracket} />
+                  Acceder <FontAwesomeIcon icon={faAngleRight} />
+                </a> */}
+                    <a
+                      href="https://m.caba.codere.bet.ar/deportes/#/HomePage"
+                      rel="nofollow"
+                    >
+                      <FontAwesomeIcon icon={faCrosshairs} />
+                      Deportes
+                      <FontAwesomeIcon icon={faAngleRight} />
+                    </a>
+                    <a
+                      href="https://m.caba.codere.bet.ar/deportes/#/DirectosPage"
+                      rel="nofollow"
+                    >
+                      <FontAwesomeIcon icon={faCirclePlay} />
+                      Directo
+                      <FontAwesomeIcon icon={faAngleRight} />
+                    </a>
+                    <a
+                      href="https://m.caba.codere.bet.ar/deportes/#/CasinoPage"
+                      rel="nofollow"
+                    >
+                      <FontAwesomeIcon icon={faCoins} />
+                      Casino
+                      <FontAwesomeIcon icon={faAngleRight} />
+                    </a>
+                    <a
+                      href="https://m.caba.codere.bet.ar/deportes/#/PromotionsPage"
+                      rel="nofollow"
+                    >
+                      <FontAwesomeIcon icon={faBullhorn} />
+                      Promociones
+                      <FontAwesomeIcon icon={faAngleRight} />
+                    </a>
+                    <a href="https://bingos.codere.bet.ar/" rel="nofollow">
+                      <FontAwesomeIcon icon={faGlobe} />
+                      Nuestras Salas
+                      <FontAwesomeIcon icon={faAngleRight} />
+                    </a>
+                    <Accordion className="hamb-accordion">
+                      <Accordion.Item eventKey="0">
+                        <Accordion.Header>¿Cómo jugar?</Accordion.Header>
+                        <Accordion.Body>
+                          <ul>
+                            <li>
+                              <a href="https://www.codere.bet.ar/casino">
+                                <FontAwesomeIcon icon={faCoins} />
+                                Casino
+                                <FontAwesomeIcon icon={faAngleRight} />
+                              </a>
+                            </li>
+                            <li>
+                              <a href="https://www.codere.bet.ar/casino/ruleta-online">
+                                <FontAwesomeIcon icon={faStar} />
+                                Rutleta
+                                <FontAwesomeIcon icon={faAngleRight} />
+                              </a>
+                            </li>
+                            <li>
+                              <a href="https://www.codere.bet.ar/casino/tragamonedas-slots-online">
+                                <FontAwesomeIcon icon={faFlag} />
+                                Slots
+                                <FontAwesomeIcon icon={faAngleRight} />
+                              </a>
+                            </li>
+                            <li>
+                              <a href="https://www.codere.bet.ar/casino/blackjack">
+                                <FontAwesomeIcon icon={faHeart} />
+                                Blackjack
+                                <FontAwesomeIcon icon={faAngleRight} />
+                              </a>
+                            </li>
+                          </ul>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    </Accordion>
+                  </Menu>
+                  <main id="page-wrap"></main>
+                </div>
+                <a
+                  href="https://m.caba.codere.bet.ar/deportes/#/HomePage"
+                  rel="nofollow"
+                >
+                  <img
+                    src={LOGO}
+                    alt="Logo Casa de Apuestas Codere - Real Madrid "
+                  />
+                </a>
+                <div id="header_buttons">
+                  {/* onClick={handleShow} */}
+                  <Button
+                    href={acceder}
+                    className="acceder-button header_btn"
+                    rel="nofollow"
+                  >
+                    Acceder
+                  </Button>
+                  <Button
+                    href={props.regis}
+                    className="registrate-button header_btn"
+                    rel="nofollow"
+                  >
+                    Registrate
+                  </Button>
+                </div>
+              </div>
+            ) : (
+              <Container>
+                <a
+                  href="https://m.caba.codere.bet.ar/deportes/#/HomePage"
+                  rel="nofollow"
+                >
+                  <img
+                    src={LOGO}
+                    alt="Logo Casa de Apuestas Codere - Real Madrid "
+                  />
+                </a>
+
+                <Nav className="me-auto">
+                  <Nav.Link
+                    href="https://m.caba.codere.bet.ar/deportes/#/HomePage"
+                    rel="nofollow"
+                  >
+                    DEPORTES
+                  </Nav.Link>
+                  <Nav.Link
+                    href="https://m.caba.codere.bet.ar/deportes/#/DirectosPage"
+                    rel="nofollow"
+                  >
+                    DIRECTO
+                  </Nav.Link>
+                  <Nav.Link
+                    href="https://m.caba.codere.bet.ar/deportes/#/CasinoPage"
+                    rel="nofollow"
+                  >
+                    CASINO
+                  </Nav.Link>
+                  <Nav.Link
+                    href="https://m.caba.codere.bet.ar/deportes/#/PromotionsPage"
+                    rel="nofollow"
+                  >
+                    PROMOCIONES
+                  </Nav.Link>
+                  <Nav.Link href="https://bingos.codere.bet.ar/" rel="nofollow">
+                    NUESTRAS SASLAS
+                  </Nav.Link>
+                </Nav>
+                <Dropdown>
+                  <Dropdown.Toggle className="header_btn">
+                    <FontAwesomeIcon icon={faAngleDown} />
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="https://www.codere.bet.ar/casino">
+                      Casino
+                    </Dropdown.Item>
+                    <Dropdown.Item href="https://www.codere.bet.ar/casino/ruleta-online">
+                      Ruleta
+                    </Dropdown.Item>
+                    <Dropdown.Item href="https://www.codere.bet.ar/casino/tragamonedas-slots-online">
+                      Slots
+                    </Dropdown.Item>
+                    <Dropdown.Item href="https://www.codere.bet.ar/casino/blackjack">
+                      Blackjack
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+                <Button
+                  href={acceder}
+                  // onClick={handleShow}
+                  className="acceder-button header_btn"
+                  rel="nofollow"
+                >
+                  Acceder
+                </Button>
+                <Button
+                  href={props.regis}
+                  className="registrate-button header_btn"
+                  rel="nofollow"
+                >
+                  Registrate
+                </Button>
+              </Container>
+            )}
+            {/* <Modal id="modal_app" show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Log-In</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <Form className="test" onSubmit={formHandler}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Control
+            type="text"
+            onChange={emailHandler}
+            placeholder="Usuario ó Correo Electrónico"
+            />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={pwHandler}
+            required
+            />
+            </Form.Group>
+            <input type="submit" className="login_btn" value="Log-In" />
+            </Form>
+            </Modal.Body>
+          </Modal> */}
+          </Navbar>
+        );
+      }}
+    </isMobileContext.Consumer>
+  );
+}
+
+export default Header;
---
+++ src/Components/MySwiper.js
@@ -0,0 +1,132 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, EffectFade, Lazy } from "swiper";
+import SlideButton from "./Slide-Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "react-bootstrap";
+import banners from "../JSON/data.json";
+
+import "swiper/css";
+import "swiper/css/lazy";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import "swiper/css/autoplay";
+import { isMobileContext } from "../App";
+import { useState } from "react";
+
+var imgs = [];
+
+function MySwiper(props) {
+  const [regisButtonText, setRegisButtonText] = useState("Regístrate");
+  const [regis, setRegis] = useState(props.regis);
+
+  const indexHandler = (swiper) => {
+    // console.log(swiper);
+    if (swiper.realIndex === 1) props.setShowTimer(true);
+    else props.setShowTimer(false);
+    if (swiper.realIndex === imgs.length - 1) {
+      {
+        setRegisButtonText("Visitanos");
+        setRegis("https://bingos.codere.bet.ar/");
+      }
+    } else {
+      setRegisButtonText("Regístrate");
+      setRegis(props.regis);
+    }
+  };
+
+  return (
+    <isMobileContext.Consumer>
+      {(isMobile) => {
+        if (!isMobile) imgs = banners.desktop_slide_list;
+        else imgs = banners.mobile_slide_list;
+
+        return (
+          <div id="carousel-section">
+            <Swiper
+              onSlideChange={(swiper) => indexHandler(swiper)}
+              modules={[Pagination, EffectFade, Autoplay, Lazy]}
+              pagination={{ clickable: true }}
+              effect={"fade"}
+              lazy={{ loadPrevNext: true, loadPrevNextAmount: 1 }}
+              autoplay={{ delay: 3250 }}
+              spaceBetween={0}
+              slidesPerView={1}
+              loop={true}
+            >
+              {imgs.map((item, index) => {
+                let d1,
+                  d2,
+                  cur = new Date();
+
+                if (item.startDate != "" && item.endDate != "") {
+                  // Schedule both Start-Date and End Date
+                  d1 = new Date(item.startDate);
+                  d2 = new Date(item.endDate);
+                } else if (item.startDate == "" && item.endDate != "") {
+                  // Schedule End-Date Without Starting
+                  d1 = new Date("01/01/1970");
+                  d2 = new Date(item.endDate);
+                } else if (item.startDate != "" && item.endDate == "") {
+                  // Schedule Start-Date Without ending
+                  d1 = new Date(item.startDate);
+                  d2 = new Date("01/01/2500");
+                } else {
+                  // No Scheduling
+                  d1 = new Date("01/01/1970");
+                  d2 = new Date("01/01/2500");
+                }
+
+                if (cur >= d1 && cur <= d2)
+                  return (
+                    <SwiperSlide key={index}>
+                      <a href={regis}>
+                        <img src={item.image} alt={item.img_alt} />
+                      </a>
+
+                      {!isMobile ? (
+                        <div id="des-reg">
+                          <Button
+                            className="central-regis gl-effect"
+                            href={regis}
+                            rel={"nofollow"}
+                          >
+                            {regisButtonText}
+                            <FontAwesomeIcon icon={faAngleRight} />
+                          </Button>
+                          <p style={{ color: "#fff" }}>
+                            Disponible únicamente dentro de CABA
+                          </p>
+                          {item.tycLink ? (
+                            <a className="tyc" href={item.tycLink}>
+                              Términos y condiciones
+                            </a>
+                          ) : null}
+                        </div>
+                      ) : (
+                        <>
+                          {item.tycLink ? (
+                            <div className="mob-tyc">
+                              <a className="tyc" href={item.tycLink}>
+                                Términos y condiciones
+                              </a>
+                            </div>
+                          ) : null}
+                        </>
+                      )}
+                    </SwiperSlide>
+                  );
+              })}
+            </Swiper>
+            {isMobile ? (
+              <>
+                <SlideButton regis={regis} regisButtonText={regisButtonText} />
+              </>
+            ) : null}
+          </div>
+        );
+      }}
+    </isMobileContext.Consumer>
+  );
+}
+export default MySwiper;
